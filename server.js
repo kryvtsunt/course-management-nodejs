fe06de9f4831edd08dfea1c68ebcdf79cd41d514
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2018-06-19 22:54:54 -0400

    updated to deploy to heroku

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin",
-        "http://tk-course-management-angular.herokuapp.com");
+        "https://tk-course-management-angular.herokuapp.com");
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
